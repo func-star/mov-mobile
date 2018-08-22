a6ea1e7a7492dc4ad50257cf206bcf3f6388c384
yangxi <599321378@qq.com>
2018-08-22 20:49:57 +0800

    chore: 重构Hammer组件

--- libsrc/hammer/hammer.vue
+++ libsrc/hammer/hammer.vue
@@ -1,156 +1,193 @@
 <template>
-    <div ref="movHammer" class="mona-hammer full" @touchstart="touchStart($event)" @touchmove="touchMove($event)" @touchend="touchEnd($event)" @touchcancel="touchcancel($event)" @scroll="scroll($event)">
-        <slot></slot>
-    </div>
+	<div ref="hammer" class="mona-hammer full">
+		<slot></slot>
+	</div>
 </template>
 <script type="text/javascript">
+	import Tool from '../tool'
 
-    export default {
-        props: {
-            touchstart: Function,
-            pan: Function,
-            panstart: Function,
-            panmove: Function,
-            panend: Function,
-            pancancel: Function,
-            tap: Function,
-            scrollAction: Function,
-        },
+	export default {
+		props: {
+			touchstart: Function,
+			pan: Function,
+			panstart: Function,
+			panmove: Function,
+			panend: Function,
+			pancancel: Function,
+			tap: Function,
+			scrollAction: Function
+		},
+		data () {
+			return {
+				isTouching: false
+			}
+		},
 
-        data () {
-            return {};
-        },
+		mounted () {
+			this.isPC = Tool.isPC()
+			if (this.isPC) {
+				this.$refs.hammer.addEventListener('mousedown', this.touchStart.bind(this), false)
+				this.$refs.hammer.addEventListener('mousemove', this.touchMove.bind(this), false)
+				this.$refs.hammer.addEventListener('mouseup', this.touchEnd.bind(this), false)
+			} else {
+				this.$refs.hammer.addEventListener('touchstart', this.touchStart.bind(this), false)
+				this.$refs.hammer.addEventListener('touchmove', this.touchMove.bind(this), false)
+				this.$refs.hammer.addEventListener('touchend', this.touchEnd.bind(this), false)
+				this.$refs.hammer.addEventListener('touchcancel', this.touchCancel.bind(this), false)
+			}
+			this.$refs.hammer.addEventListener('scroll', this.scroll.bind(this), false)
+		},
 
-        methods: {
-            getInfo (prev, next) {
-                let angle;
-                if (prev.pageY == next.pageY) {
-                    angle = prev.pageX - next.pageX < 0 ? 0 : 180;
-                } else if (prev.pageX == next.pageX) {
-                    angle = prev.pageY - next.pageY < 0 ? -90 : 90;
-                } else {
-                    angle = 180 / (Math.PI / Math.atan((prev.pageY - next.pageY) / (prev.pageX - next.pageX)));
-                }
-                return {
-                    velocityY: (next.pageY - prev.pageY) / (next.time - prev.time),
-                    velocityX: (next.pageX - prev.pageX) / (next.time - prev.time),
-                    deltaY: next.pageY - this.startInfo.pageY,
-                    deltaX: next.pageX - this.startInfo.pageX,
-                    angle: angle,
-                };
-            },
-            setInfo (e, isStart) {
-                let now = Date.now();
+		methods: {
+			optionEvent (e) {
+				if (this.isPC) {
+					return {
+						x: e.clientX,
+						y: e.clientY
+					}
+				} else {
+					return {
+						x: e.touches[0].pageX,
+						y: e.touches[0].pageY
+					}
+				}
+			},
 
-                let pageY;
-                let pageX;
+			getInfo (prev, next) {
+				let angle
+				if (prev.pageY === next.pageY) {
+					angle = prev.pageX - next.pageX < 0 ? 0 : 180
+				} else if (prev.pageX === next.pageX) {
+					angle = prev.pageY - next.pageY < 0 ? -90 : 90
+				} else {
+					angle = 180 / (Math.PI / Math.atan((prev.pageY - next.pageY) / (prev.pageX - next.pageX)))
+				}
+				return {
+					velocityY: (next.pageY - prev.pageY) / (next.time - prev.time),
+					velocityX: (next.pageX - prev.pageX) / (next.time - prev.time),
+					deltaY: next.pageY - this.startInfo.pageY,
+					deltaX: next.pageX - this.startInfo.pageX,
+					angle: angle
+				}
+			},
 
-                let nextData = {
-                    pageY: e.touches[0].pageY,
-                    pageX: e.touches[0].pageX,
-                    time: now,
-                };
+			setInfo (e, isStart) {
+				let pageX, pageY
+				let now = Date.now()
 
-                if (isStart) {
-                    pageX = this.startInfo.pageX;
-                    pageY = this.startInfo.pageY;
-                } else {
-                    pageX = this.prevInfo.pageX;
-                    pageY = this.prevInfo.pageY;
-                }
+				const ePoint = this.optionEvent(e)
+				const nextData = {
+					pageY: ePoint.y,
+					pageX: ePoint.x,
+					time: now
+				}
 
-                let preData = {
-                    pageY: pageY,
-                    pageX: pageX,
-                    time: this.prevInfo.time,
-                };
+				if (isStart) {
+					pageX = this.startInfo.pageX
+					pageY = this.startInfo.pageY
+				} else {
+					pageX = this.prevInfo.pageX
+					pageY = this.prevInfo.pageY
+				}
 
-                let info = this.getInfo(preData, nextData);
-                info.center = {
-                    y: e.touches[0].pageY,
-                    x: e.touches[0].pageX,
-                };
+				const preData = {
+					pageY: pageY,
+					pageX: pageX,
+					time: this.prevInfo.time
+				}
 
-                this.prevInfo = nextData;
-                this.currentInfo = Object.assign({}, info);
+				let info = this.getInfo(preData, nextData)
+				info.center = {
+					y: ePoint.y,
+					x: ePoint.x
+				}
 
-                Object.assign(e, info);
-            },
-            touchStart (e) {
-                this.prevInfo = {
-                    time: Date.now(),
-                    pageY: e.touches[0].pageY,
-                    pageX: e.touches[0].pageX,
-                };
-                this.startInfo = {
-                    time: Date.now(),
-                    pageY: e.touches[0].pageY,
-                    pageX: e.touches[0].pageX,
-                };
-                this.touchstart && this.touchstart(e);
-                this.pressTimer = setTimeout(() => {
-                    this.hammerPress(e);
-                }, 500);
-            },
+				this.prevInfo = nextData
+				this.currentInfo = Object.assign({}, info)
 
-            touchMove (e) {
-                let isStart = !this.currentInfo;
-                clearTimeout(this.pressTimer);
-                this.isPan = true;
-                this.setInfo(e, isStart);
-                if (this.isPan) {
-                    if (isStart && this.pan) {
-                        this.pan(e);
-                    }
-                    if (isStart && this.panstart) {
-                        this.panstart(e);
-                    }
-                    if (this.panmove) {
-                        this.panmove(e);
-                    }
-                }
-            },
-            touchEnd (e) {
-                Object.assign(e, this.currentInfo);
-                clearTimeout(this.pressTimer);
-                let duration = Date.now() - this.startInfo.time;
+				Object.assign(e, info)
+			},
 
-                this.startInfo = null;
-                this.prevInfo = null;
-                this.currentInfo = null;
+			touchStart (e) {
+				this.isTouching = true
+				const ePoint = this.optionEvent(e)
+				this.prevInfo = {
+					time: Date.now(),
+					pageY: ePoint.y,
+					pageX: ePoint.x
+				}
+				this.startInfo = {
+					time: Date.now(),
+					pageY: ePoint.y,
+					pageX: ePoint.x
+				}
+				this.touchstart && this.touchstart(e)
+				clearTimeout(this.pressTimer)
+				this.pressTimer = setTimeout(() => {
+					this.hammerPress(e)
+				}, 500)
+			},
 
-                if (this.isPan) {
-                    this.pan && this.pan(e);
-                    this.panend && this.panend(e);
-                } else {
-                    if (duration < 250) {
-                        this.hammerTap(e);
-                    }
-                }
-                this.isPan = false;
-            },
-            touchCancel (e) {
-                this.startInfo = null;
-                this.prevInfo = null;
-                this.currentInfo = null;
-                this.isPan = false;
-                clearTimeout(this.pressTimer);
-                if (this.isPan) {
-                    this.pancancel && this.pancancel(e);
-                }
-            },
-            scroll (e) {
-                this.scrollAction && this.scrollAction(e);
-            },
-            hammerTap (e) {
-                this.tap && this.tap(e);
-            },
-            hammerPress (e) {
-                this.press && this.press(e);
-            },
-        },
+			touchMove (e) {
+				if (!this.isTouching) {
+					return
+				}
+				clearTimeout(this.pressTimer)
 
-        mounted () {},
-    };
+				// 是否为touchmove的第一次动作
+				const isStart = !this.currentInfo
+				// 是否触发了touchmove的第一次动作
+				this.isPan = true
+				this.setInfo(e, isStart)
+				isStart && this.pan && this.pan(e)
+				isStart && this.panstart && this.panstart(e)
+				this.panmove && this.panmove(e)
+			},
+
+			touchEnd (e) {
+				if (!this.isTouching) {
+					return
+				}
+				this.isTouching = false
+				Object.assign(e, this.currentInfo)
+				clearTimeout(this.pressTimer)
+
+				const duration = Date.now() - this.startInfo.time
+				this.startInfo = null
+				this.prevInfo = null
+				this.currentInfo = null
+
+				if (this.isPan) {
+					this.pan && this.pan(e)
+					this.panend && this.panend(e)
+				} else {
+					// 没有移动，没有触发touchmove
+					(duration < 250) && this.hammerTap(e)
+				}
+				this.isPan = false
+			},
+
+			touchCancel (e) {
+				this.startInfo = null
+				this.prevInfo = null
+				this.currentInfo = null
+				this.isPan = false
+				clearTimeout(this.pressTimer)
+				this.pancancel && this.pancancel(e)
+			},
+
+			scroll (e) {
+				this.scrollAction && this.scrollAction(e)
+			},
+
+			hammerTap (e) {
+				this.tap && this.tap(e)
+			},
+
+			hammerPress (e) {
+				this.press && this.press(e)
+			}
+		}
+	}
 
 </script>
