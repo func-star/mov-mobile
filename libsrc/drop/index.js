26fef24966e3beab68b014aa5d56c067b9748ff1
yangxi <599321378@qq.com>
2018-08-29 20:22:22 +0800

    feat: 修改主页和logo

--- libsrc/drop/index.js
+++
@@ -1,8 +0,0 @@
-import Drop from './drop.vue'
-import DropTitle from './title.vue'
-import DropContent from './content.vue'
-
-Drop.Title = DropTitle
-Drop.Content = DropContent
-
-export default Drop
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,34 +1,27 @@
 <template>
 	<div class="home-demo">
-		<Drop>
-			<DropTitle>title</DropTitle>
-			<DropContent>content</DropContent>
-		</Drop>
-		<a class="demo-item d-b" :href="item.url" v-for="(item, index) in list">
-			<div class="name">{{item.name}}</div>
-			<div class="desc">{{item.desc}}</div>
-		</a>
+		<h1 class="home-demo-logo flex-center">
+			<img src="http://static.monajs.cn/common/logo100.png" />
+			<span>Mov</span>
+		</h1>
+		<div class="home-demo-desc text-center">让你感到幸福的 Vue 组件库</div>
+		<div class="home-demo-wrap">
+			<a class="demo-item d-b" :href="item.url" v-for="(item, index) in list">
+				<div class="name">{{item.name}}</div>
+				<div class="desc">{{item.desc}}</div>
+			</a>
+		</div>
 	</div>
 </template>
 <script type="text/javascript">
 	import ListConfig from './list.config'
-	import { Drop } from 'mona'
 	import './index.less'
-
-	const DropTitle = Drop.Title
-	const DropContent = Drop.Content
 
 	export default {
 		data () {
 			return {
 				list: []
 			}
-		},
-
-		components: {
-			Drop,
-			DropTitle,
-			DropContent
 		},
 
 		mounted () {
