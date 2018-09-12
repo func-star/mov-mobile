3160bfe347a5b6f8138d4de01aa3d698ab930302
santi <>
2018-09-12 16:49:39 +0800

    tabs隔离数据，修复多个tabs共用同一份数据的问题

--- libsrc/tabs/ctrl.js
+++ libsrc/tabs/ctrl.js
@@ -1,18 +1,27 @@
 import Events from 'mona-events';
 
 class TabsCtrl extends Events {
-    itemKeyList = []; // 控制tabsItem的加载
-    
-    setTabItemKey (key) {
-        if (this.itemKeyList.indexOf(key) !== -1) {
-            return;
-        }
-        this.itemKeyList.push(key);
-    }
-    
-    getTabsItemKey () {
-        return this.itemKeyList;
-    }
+	itemKeyInfo = {}; // 控制tabsItem的加载
+
+	setTabItemKey(tabsKey, key) {
+		if (!this.itemKeyInfo['itemKeyList' + tabsKey]) {
+			this.itemKeyInfo['itemKeyList' + tabsKey] = [];
+		}
+		if (this.itemKeyInfo['itemKeyList' + tabsKey].indexOf(key) !== -1) {
+			return;
+		}
+		this.itemKeyInfo['itemKeyList' + tabsKey].push(key);
+	}
+
+	getTabsItemKey(tabsKey) {
+		const key = this.itemKeyInfo['itemKeyList' + tabsKey];
+		return key ? key : [];
+	}
+
+	destroyItemList(tabsKey) {
+		delete this.itemKeyInfo['itemKeyList' + tabsKey];
+	}
+
 }
 
 export default new TabsCtrl;
