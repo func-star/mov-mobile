1cf47f8ec530c1997daf8e4d78b213045abc4633
yingdian <18268394667@163.com>
2018-09-17 10:48:34 +0800

    fix: 修复tabs和swiper的item计算时无高度不占位问题

--- libsrc/tabs/swiper/item.vue
+++ libsrc/tabs/swiper/item.vue
@@ -1,48 +1,49 @@
 <template lang="html">
-    <div class="mona-swiper-item pull-left h-full pos-r" :style="itemSty">
-        <slot v-if="isShow"></slot>
-    </div>
+	<div class="mona-swiper-item pull-left h-full pos-r" :style="itemSty">
+		<slot v-if="isShow"></slot>
+	</div>
 </template>
 <script>
-import Tabs from '../tabs.vue';
-import TabsCtrl from '../ctrl';
+	import Tabs from '../tabs.vue';
+	import TabsCtrl from '../ctrl';
 
-export default {
-	props: {
-	    tabsKey: Number
-	},
-    data() {
-        return {
-            itemWidth: 0,
-            isShow: false,
-            key: `tabsItem${this._uid}`,
-        };
-    },
-    computed: {
-        itemSty(){
-            return {
-                width: this.itemWidth + 'px',
-            };
-        },
-    },
-    created() {
-        console.log(`设置tabsItem的唯一key${this.key}`);
-        TabsCtrl.setTabItemKey(this.tabsKey, this.key);
-    },
-    beforeDestroy() {
-        TabsCtrl.off('tabsIndexChange');
-    },
-    mounted() {
-        this.$nextTick(() => {
-            this.itemWidth = this.$parent.$parent.itemWidth;
-        });
-        TabsCtrl.on('tabsIndexChange', (keysList)=>{
-            if(this.isShow) {
-                return;
-            }
-            this.isShow = keysList.has(this.key);
-        });
-    },
-};
+	export default {
+		props: {
+			tabsKey: Number,
+		},
+		data() {
+			return {
+				itemWidth: 0,
+				isShow: false,
+				key: `tabsItem${this._uid}`,
+			};
+		},
+		computed: {
+			itemSty() {
+				return {
+					width: this.itemWidth + 'px',
+					minHeight: '1px',
+				};
+			},
+		},
+		created() {
+			console.log(`设置tabsItem的唯一key${this.key}`);
+			TabsCtrl.setTabItemKey(this.tabsKey, this.key);
+		},
+		beforeDestroy() {
+			TabsCtrl.off('tabsIndexChange');
+		},
+		mounted() {
+			this.$nextTick(() => {
+				this.itemWidth = this.$parent.$parent.itemWidth;
+			});
+			TabsCtrl.on('tabsIndexChange', (keysList) => {
+				if (this.isShow) {
+					return;
+				}
+				this.isShow = keysList.has(this.key);
+			});
+		},
+	};
 
 </script>
